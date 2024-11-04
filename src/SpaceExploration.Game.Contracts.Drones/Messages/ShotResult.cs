df02fd740bb680f30025f67f4ed9e8b2612542b5
Tobias Richling <richlint@apetito.com>
2024-11-04 11:59:56 +0100

    fix shot result error

--- src/SpaceExploration.Game.Contracts.Drones/Messages/ShotResult.cs
+++ src/SpaceExploration.Game.Contracts.Drones/Messages/ShotResult.cs
@@ -2,6 +2,10 @@ namespace SpaceExploration.Game.Contracts.Drones.Messages;
 
 public record ShotResult(Guid DroneId, ShotOutcome Outcome, Guid? TargetId, Guid? TargetSignature)
 {
+    public ShotResult() : this(Guid.Empty, ShotOutcome.ShootingDroneNotFound, null, null)
+    {
+    }
+
     public ShotResult(Guid droneId, ShotOutcome outcome) : this(droneId, outcome, null, null)
     {
     }
